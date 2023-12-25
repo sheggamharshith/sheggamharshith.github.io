414b9913935acdde3d2f9cee4953a98aa626956b
sheggamharshith <harshit675@gmail.com>
2023-12-25 13:50:09 -0500

    feat: :sparkles: added the next js config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+const nextConfig = {
+  output: 'export',
+}
+module.exports = {
+  ...withNextra(),
+  ...nextConfig()
+}
