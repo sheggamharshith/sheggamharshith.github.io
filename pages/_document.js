d6eb23dc80431b90219753e42180dc9b73eac02c
sheggamharshith <harshit675@gmail.com>
2023-12-28 11:45:26 -0500

    feat: :bug: updated more meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,22 @@ export default function Document() {
   const meta = {
     title: 'Harshith Sheggam',
     description: 'Harshith Sheggam Portfolio',
-    image: ''
+    image: '',
+    author: 'Harshith Sheggam',
+    date:  ''
   }
 
   return (
     <Html lang="en">
       <Head>
         <meta name="robots" content="follow, index" />
-        <meta name='robots' content="max-image-preview:large"/>
+        <meta name="robots" content="max-image-preview:large" />
         <meta name="description" content={meta.description} />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
+        <meta name="author" content={meta.author} />
+        <meta name="date" content={meta.date} />
         {/* <meta property="og:image" content={meta.image} /> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@harshit675" />
